223bc5d7a3f7df0e10e2b04ec9c4cb77efd2d504
ilorez <zobirnajdaoui@gmail.com>
2024-08-13 13:54:26 +0100

    start in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
-
+import Header from "@/components/global/homepage/header";
+// create a home page for my portfolio
 export default function Home() {
   return (
     <div>
       Home
     <Button > Hello World!</Button> 
+    <Header />
     </div>
   );
 }
---
+++ components/global/homepage/header.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+  return (
+    <header className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold">Welcome to my Portfolio</h1>
+      <p className="text-xl">This is a simple portfolio site built with Next.js and Tailwind CSS</p>
+    </header>
+  );
+  }
