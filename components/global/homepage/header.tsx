ffa94506036735705ab9eae95784fc2b56963679
ilorez <zobirnajdaoui@gmail.com>
2024-08-15 21:00:41 +0100

    uploaded necessary packages

--- components/global/homepage/header.tsx
+++ components/global/homepage/header.tsx
@@ -2,9 +2,9 @@
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-6xl font-bold">Welcome to my Portfolio</h1>
-      <p className="text-xl">This is a simple portfolio site built with Next.js and Tailwind CSS</p>
-    </header>
+    <div className="">
+      <h1 className="">Welcome to my Portfolio</h1>
+      <p className="">This is a simple portfolio site built with Next.js and Tailwind CSS</p>
+    </div>
   );
   }
