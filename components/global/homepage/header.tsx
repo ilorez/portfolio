26cae9bae7b88a65f7cad915baa86abff0ff857b
ilorez <zobirnajdaoui@gmail.com>
2024-08-15 21:42:41 +0100

    working on header

--- components/global/homepage/header.tsx
+++ components/global/homepage/header.tsx
@@ -1,10 +1,36 @@
-
+import Image from 'next/image';
+import github_icon from '/public/icons/social/github.svg';
 
 export default function Header() {
   return (
     <div className="">
-      <h1 className="">Welcome to my Portfolio</h1>
-      <p className="">This is a simple portfolio site built with Next.js and Tailwind CSS</p>
+      <div>
+        <div>
+          <span>Najdaoui Zobair</span>
+          <span>Full-Stack Developer</span>
+        </div>
+        <div>
+          <div>{/* image */}</div>
+          <div>
+            {/* links */}
+            <ul>
+              {/* github */}
+              <li>
+                <a href="https://github.com/ilorez" target="_blank">
+                  <Image
+                    src={github_icon}
+                    alt="Github"
+                    width={24}
+                    height={24}
+                    // change svg color to black
+                    className="fill-current text-green-500"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
-  }
+}
