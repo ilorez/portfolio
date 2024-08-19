8e898c48e3b243bf87e9c9ff3930132b19303e71
ilorez <zobirnajdaoui@gmail.com>
2024-08-19 20:54:51 +0100

    feat: Add Navbar component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import Header from "@/components/global/homepage/header";
+import Navbar from "@/components/global/Navbar";
 // create a home page for my portfolio
 export default function Home() {
   return (
-    <div>
+    <div className="h-[200vh]">
       <Header />
+      <div className="fixed top-0">
+
+      <Navbar />
+      </div>
     </div>
   );
 }
