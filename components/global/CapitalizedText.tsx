2a9bf7ff22e0722f58cad30f27f91751e546f217
ilorez <zobirnajdaoui@gmail.com>
2024-08-20 21:09:49 +0100

    feat: Add CapitalizedText component

---
+++ components/global/CapitalizedText.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const CapitalizedText = ({ text }: { text: string }) => {
+  const words = text.split(' ');
+  const capitalizedWords = words.map((word) => {
+    const firstLetter = word.charAt(0).toUpperCase();
+    const restOfWord = word.slice(1);
+    return (
+      <span key={word}>
+        <span className="text-primary font-bold">{firstLetter}</span>
+        {restOfWord}
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      {capitalizedWords.map((word, index) => (
+        <span key={index}>{word} </span>
+      ))}
+    </div>
+  );
+};
+
+export default CapitalizedText;
