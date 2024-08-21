b9f25e4c2acc17e7258cd8163551f717645fdcb6
ilorez <zobirnajdaoui@gmail.com>
2024-08-21 07:37:20 +0100

    refactor: Update VerticalLine component styling

--- components/global/VerticalLine.tsx
+++ components/global/VerticalLine.tsx
@@ -8,7 +8,8 @@ interface VerticalLineProps {
 const VerticalLine = () => {
   return (
     <div className="flex items-center justify-center h-full">
-      <div className="w-2 h-full rounded-full bg-gradient-to-b to-[#3c832f] from-[#8ffbd3] shadow-[0_0_12px_0_rgba(0,255,0,0.5)]"></div>
+      <div className="w-2 h-full rounded-full bg-gradient-to-b from-[#93D1BE] via-[#2EB88E] to-[#93D1BE]"></div>
+      {/* bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 */}
     </div>
   );
 };
