999343cce3935969cab2f65ff20794aa763f9b1b
ilorez <zobirnajdaoui@gmail.com>
2024-08-25 12:17:45 +0100

    feat: Add LucideIcon component for lazy loading and rendering Lucide icons

---
+++ components/global/LucideIcon.tsx
@@ -0,0 +1,21 @@
+import { lazy, Suspense } from 'react';
+import { LucideProps } from 'lucide-react';
+import dynamicIconImports from 'lucide-react/dynamicIconImports';
+
+const fallback = <div style={{ background: '#ddd', width: 16, height: 16 }}/>
+
+interface IconProps extends Omit<LucideProps, 'ref'> {
+  name: keyof typeof dynamicIconImports;
+}
+
+const Icon = ({ name, ...props }: IconProps) => {
+  const LucideIcon = lazy(dynamicIconImports[name]);
+
+  return (
+    <Suspense fallback={fallback}>
+      <LucideIcon {...props} />
+    </Suspense>
+  );
+}
+
+export default Icon
