d648cee30c9ac2d3d7fb69040ecd5b85afcbee42
ilorez <zobirnajdaoui@gmail.com>
2024-08-25 23:33:09 +0100

    refactor: Update VerticalLine component to accept a className prop

--- components/global/VerticalLine.tsx
+++ components/global/VerticalLine.tsx
@@ -1,19 +1,17 @@
+import { cn } from '@/lib/utils';
 import React from 'react';
 
 interface VerticalLineProps {
-  from: string;
-  via: string;
-  to: string;
-  width ?: number;
+  className?: string;
 }
 
-const VerticalLine = ({from,via,to,width = 6 }:VerticalLineProps) => {
+const VerticalLine = ({ className }: VerticalLineProps) => {
   return (
     <div className="flex items-center justify-center h-full">
       {/* <div className="w-2 h-full rounded-full bg-gradient-to-b from-[#93D1BE] via-[#2EB88E] to-[#93D1BE]"></div> */}
-      <div className={
-        `w-[5px] h-full rounded-full bg-gradient-to-b from-[${from}] [via-${via}] to-[${to}]`
-      }></div>
+      <div
+        className={cn(`w-vertical-line h-full rounded-full `, className)}
+      ></div>
       {/* bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 */}
     </div>
   );
