5b190564a7fc18868f28404e7cfa8bac8d3b5284
ilorez <zobirnajdaoui@gmail.com>
2024-08-25 23:32:48 +0100

    refactor: Update page layout to fix About component positioning

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,42 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
-import { Inter as FontSans } from "next/font/google"
- 
-import { cn } from "@/lib/utils"
- 
+import { Inter as FontSans } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+  subsets: ['latin'],
+  variable: '--font-sans'
+});
 
 export const metadata: Metadata = {
-  title: "Ilorez",
-  description: "ilorez portfolio",
+  title: 'Ilorez',
+  description: 'ilorez portfolio'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body 
-className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
           fontSans.variable
         )}
-    >{children}</body>
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
