4c53d73e37c82d07e29083fe3044eae5cc2bbeb6
ilorez <zobirnajdaoui@gmail.com>
2024-08-27 20:15:59 +0100

    refactor: Remove unused imports and update component structure in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 'use client';
-
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
 import Header from '@/components/global/homepage/header';
 import Navbar from '@/components/global/Navbar';
-import VerticalLine from '@/components/global/VerticalLine';
 import About from '@/components/global/homepage/About';
-import Boxes from '@/components/test';
 // create a home page for my portfolio
 export default function Home() {
   return (
@@ -17,10 +12,6 @@ export default function Home() {
       </div>
       <div className="m-6 ">
         <About />
-      </div>
-      <div className='w-full flex items-center justify-center'>
-
-      <Boxes />
       </div>
     </div>
   );
