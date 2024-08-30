6ccb86c4e1598b542e8c3d02695cb489b2880e08
ilorez <zobirnajdaoui@gmail.com>
2024-08-30 22:47:39 +0100

    refactor: Update VerticalLine component with shadow styling

--- components/global/VerticalLine.tsx
+++ components/global/VerticalLine.tsx
@@ -6,15 +6,13 @@ interface VerticalLineProps {
 }
 
 const VerticalLine = ({ className }: VerticalLineProps) => {
-  return (
-    <div className="flex items-center justify-center h-full">
-      {/* <div className="w-2 h-full rounded-full bg-gradient-to-b from-[#93D1BE] via-[#2EB88E] to-[#93D1BE]"></div> */}
-      <div
-        className={cn(`w-vertical-line h-full rounded-full `, className)}
-      ></div>
-      {/* bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 */}
-    </div>
+return (
+ <div className="flex items-center justify-center h-full">
+ <div
+ className={cn(`w-vertical-line h-full rounded-full shadow-xl shadow-i-about-from/50`, className)}
+ ></div>
+ </div>
   );
-};
+ };
 
 export default VerticalLine;
