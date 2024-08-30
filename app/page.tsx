0ce908fd6d624415dd672b0f7cbd88645cac9641
ilorez <zobirnajdaoui@gmail.com>
2024-08-30 22:47:59 +0100

    chore: Update page layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,14 @@ export default function Home() {
   return (
     <div className="h-[200vh]">
       <Header />
-      <div className="fixed top-0 m-6 z-10">
+      <div className="fixed top-0 m-6  z-10">
         <Navbar />
       </div>
-      <div className="m-6 ">
+      <div className="flex flex-col m-6 w-fit items-start mt-6">
+
+      <div className="w-full max-w-[800px]  text-lg">
         <About />
+      </div>
       </div>
     </div>
   );
