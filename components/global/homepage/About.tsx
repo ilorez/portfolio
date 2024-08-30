988bc5827ce0e9a38dbd24c8f7111cb8faef73b4
ilorez <zobirnajdaoui@gmail.com>
2024-08-30 22:49:00 +0100

    refactor: Update About component styling and add radial gradient background

--- components/global/homepage/About.tsx
+++ components/global/homepage/About.tsx
@@ -9,16 +9,18 @@ const About: React.FC = () => {
   return (
     <div className="flex flex-col pl-6 w-full">
       {/* about */}
-      <div className="flex flex-row gap-6 h-screen">
+      <div className="flex flex-row gap-6 h-fit">
         {/* the line */}
         <div className="flex flex-col items-center justify-center gap-2">
           <div className="relative inline-block">
+            {/* change this to classnames radial-gradient(circle, #506A64 0%, #2B3331 40%) */}
+            {/*bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 */}
             <div
-              className="absolute -z-10 inset-0 rounded-full"
-              style={{
-                background: `radial-gradient(circle, #506A64 0%, #2B3331 40%)`,
-                transform: 'scale(2.40)' // Makes the shadow slightly larger than the icon
-              }}
+              className="absolute -z-10 inset-0 rounded-full scale-125 shadow-lg shadow-i-about-from/50  "
+              // style={{
+              //   background: `radial-gradient(circle, #506A64 0%, #2B3331 40%)`,
+              //   transform: 'scale(2.40)' // Makes the shadow slightly larger than the icon
+              // }}
             ></div>
             <div className="relative">
               <User size={30} />
@@ -31,7 +33,7 @@ const About: React.FC = () => {
           {/* about me */}
           <div className="flex flex-col gap-2">
             <CapitalizedText text="About Me" />
-            <p>
+            <p className='text-justify'>
               I&apos;m{' '}
               <span className={cn('text-primary', thirdFont.className)}>
                 Zobair Najdaoui
