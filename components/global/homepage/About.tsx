d739740a675acdae665aac227969a273c4b9d735
ilorez <zobirnajdaoui@gmail.com>
2024-09-02 20:05:44 +0100

    refactor: Update About component to use relative imports for Services, FunFact, and IconWithVerticalLine

--- components/global/homepage/About.tsx
+++ components/global/homepage/About.tsx
@@ -14,9 +14,8 @@ const About: React.FC = () => {
   const services: Service[] = data.services as Service[];
   const fun_facts: FunFact[] = data.fun_facts as FunFact[];
   return (
-    <div className="flex flex-col pl-6 w-full gap-8">
-      {/* about */}
-      <div className="flex flex-row gap-6 h-fit">
+    <div className="flex flex-row gap-6 h-fit">
+        {/* about */}
         {/* the line */}
         <IconWithVerticalLine
           icon="user"
@@ -28,7 +27,11 @@ const About: React.FC = () => {
         <div className="flex flex-col gap-5">
           {/* about me */}
           <div className="flex flex-col gap-2">
-            <CapitalizedText text="About Me" cap_color='text-primary' light_color='text-light-primary' />
+            <CapitalizedText
+              text="About Me"
+              cap_color="text-primary"
+              light_color="text-light-primary"
+            />
             <p className="text-justify text-lg max-w-[800px]">
               I&apos;m{' '}
               <span className={cn('text-primary', thirdFont.className)}>
@@ -43,7 +46,11 @@ const About: React.FC = () => {
           </div>
           {/* services */}
           <div className="flex flex-col gap-2">
-            <CapitalizedText text="Services" cap_color='text-primary' light_color='text-light-primary' />
+            <CapitalizedText
+              text="Services"
+              cap_color="text-primary"
+              light_color="text-light-primary"
+            />
             <div className="flex flex-wrap w-full gap-2">
               {services.map((service, index) => (
                 <Services
@@ -57,7 +64,11 @@ const About: React.FC = () => {
           </div>
           {/* fun fucts */}
           <div className="flex flex-col gap-2">
-            <CapitalizedText text="Fun Facts" cap_color='text-primary' light_color='text-light-primary' />
+            <CapitalizedText
+              text="Fun Facts"
+              cap_color="text-primary"
+              light_color="text-light-primary"
+            />
             <div className="flex flex-wrap w-full gap-2">
               {fun_facts.map((fun_fact, index) => (
                 <FunFacts
@@ -70,37 +81,7 @@ const About: React.FC = () => {
           </div>
         </div>
       </div>
-      {/* services */}
-      <div className="flex flex-row gap-6 h-fit">
-        {/* the line */}
-        <IconWithVerticalLine
-          icon="briefcase-business"
-          iconSize={30}
-          shadow="shadow-i-experience-from/50"
-          className="bg-gradient-to-b from-i-experience-from via-i-experience-via to-i-experience-to"
-        />
-        {/* content */}
-        <div className="flex flex-col gap-5">
-          {/* about me */}
-          <div className="flex flex-col gap-2">
-            <CapitalizedText text="experience" cap_color='text-i-experience-via' light_color='text-light-experience' />
-            <p className="text-justify text-lg max-w-[800px]">
-              I&apos;m{' '}
-              <span className={cn('text-primary', thirdFont.className)}>
-                Zobair Najdaoui
-              </span>
-              , a full-stack developer from Morocco with a knack for design. I
-              love running and traveling, finding joy in the simplicity of a
-              good run and the adventure of exploring new places. Combining my
-              technical skills with my passions, I strive to create engaging
-              digital experiences that leave a mark.
-            </p>
-          </div>
-          
-          
-        </div>
-      </div>
-    </div>
+
   );
 };
 
