920db8eb91e88e922c1b6b1f327cd080bcf313c7
ilorez <zobirnajdaoui@gmail.com>
2024-09-02 20:05:51 +0100

    refactor: Update Experience component to use ExperienceCard for displaying experiences

--- components/global/homepage/Exprience.tsx
+++ components/global/homepage/Exprience.tsx
@@ -3,31 +3,33 @@ import CapitalizedText from '../CapitalizedText';
 // import json data
 import data from '@/IlorezData/ilorez.json';
 import IconWithVerticalLine from '../IconWithVerticalLine';
+import ExperienceCard, { ExperienceProps } from '../ExperieceCard';
 
 const Experience: React.FC = () => {
-  const experices= "get experices type and data from ilorez data"
+  const experiences: ExperienceProps[] = data.experiences as ExperienceProps[];
   return (
-    <div className="flex flex-col pl-6 w-full gap-8">
-      {/* services */}
-      <div className="flex flex-row gap-6 h-fit">
-        {/* the line */}
-        <IconWithVerticalLine
-          icon="briefcase-business"
-          iconSize={30}
-          shadow="shadow-i-experience-from/50"
-          className="bg-gradient-to-b from-i-experience-from via-i-experience-via to-i-experience-to"
-        />
-        {/* content */}
-        <div className="flex flex-col gap-5">
-          {/* Experiences */}
-          <div className="flex flex-col gap-2">
-            <CapitalizedText text="experience" cap_color='text-i-experience-via' light_color='text-light-experience' />
-            <div>
-              woow
-            </div>
+    <div className="flex flex-row gap-6 h-fit">
+      {/* the line */}
+      <IconWithVerticalLine
+        icon="briefcase-business"
+        iconSize={30}
+        shadow="shadow-i-experience-from/50"
+        className="bg-gradient-to-b from-i-experience-from via-i-experience-via to-i-experience-to"
+      />
+      {/* content */}
+      <div className="flex flex-col gap-5">
+        {/* Experiences */}
+        <div className="flex flex-col gap-2">
+          <CapitalizedText
+            text="experience"
+            cap_color="text-i-experience-via"
+            light_color="text-light-experience"
+          />
+          <div>
+            {experiences.map((exp, index) => (
+              <ExperienceCard key={index} {...exp} />
+            ))}
           </div>
-          
-          
         </div>
       </div>
     </div>
